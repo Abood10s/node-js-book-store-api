1a94123723cf757f7bd7082cba3bd4dfe695e52c
Abood10s <saafeenabood@gmail.com>
2023-08-22 16:33:26 +0300

    sending reset password link to email with gmail service in nodemailer

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const { User, validateChangePassword } = require("../models/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-// const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 
 /**
  *  @desc    Get Forgot Password View
@@ -37,7 +37,31 @@ module.exports.sendForgotPasswordLink = asyncHandler(async (req, res) => {
   });
   //reset link expires in 10 minute as the token specifies
   const link = `http://localhost:5000/password/reset-password/${user._id}/${token}`;
-  res.json({ message: "Click on the link", resetPasswordLink: link });
+  //email sending reset link config
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.USER_EMAIL,
+      pass: process.env.USER_PASS,
+    },
+  });
+  const mailOptions = {
+    from: process.env.USER_EMAIL,
+    to: user.email,
+    subject: "Reset Password",
+    html: `<div>  
+      <h4>Click on the link below to reset your password</h4>
+      <p>${link}</p>
+    </div>`,
+  };
+  transporter.sendMail(mailOptions, function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent", success.response());
+    }
+  });
+  res.render("link-send");
 });
 
 /**
